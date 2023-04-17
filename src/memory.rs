da5ce04da145f9cf80d4f457f5b7f49191e5eda6
Estus <git@estus.dev>
2023-04-17 16:06:55 -0600

    feat: derive Copy and Clone for Address

--- src/memory.rs
+++ src/memory.rs
@@ -2,7 +2,7 @@ use std::{fmt, fmt::Debug, fmt::Display, fmt::Formatter};
 
 /// The CHIP-8 has 12-bit addresses, allowing up to 4096 bytes of memory.
 /// https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Technical-Reference#storage-in-memory
-#[derive(Default, PartialEq, Eq)]
+#[derive(Copy, Clone, Default, PartialEq, Eq)]
 pub struct Address(u16);
 
 impl Address {
