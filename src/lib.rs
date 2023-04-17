4df1d1af88b72eebcd4629888657e4f18f5b7a37
Estus <git@estus.dev>
2023-04-16 20:40:17 -0600

    refactor: move chip8 back to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,31 @@
-mod chip8;
 mod instructions;
 mod registers;
 
 pub mod prelude {
-    pub use crate::chip8::Chip8;
     pub use crate::instructions::Instruction;
     pub use crate::registers::{Register, Register::*, Registers};
+    pub use crate::Chip8;
+}
+
+use crate::prelude::*;
+use std::{fmt, fmt::Display};
+
+/// Chip8 represents the current state of the entire machine.
+/// https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Technical-Reference
+#[derive(Default, Debug)]
+pub struct Chip8 {
+    /// The CHIP-8 has 16 8-bit general-purpose registers, V0-VF.
+    /// https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Technical-Reference#data-registers
+    pub registers: Registers,
+}
+
+impl Chip8 {}
+
+impl Display for Chip8 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "CHIP-8")?;
+        write!(f, "\tRegisters: {}", self.registers)?;
+
+        Ok(())
+    }
 }
