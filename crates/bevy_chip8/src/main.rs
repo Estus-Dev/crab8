6361837a3f52a7f55a762614c6e7f6c7591fa7db
Estus <git@estus.dev>
2023-04-21 16:42:25 -0600

    feat: insert Chip8 as a bevy Resource

--- crates/bevy_chip8/src/main.rs
+++ crates/bevy_chip8/src/main.rs
@@ -1,5 +1,9 @@
-use bevy::prelude::*;
+use bevy::{app::App, DefaultPlugins};
+use chip_8::Chip8;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(Chip8::default())
+        .run();
 }
