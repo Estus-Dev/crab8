7ccaf37931b024efa68253e6b76a81ac5acd442f
Estus <git@estus.dev>
2023-04-20 18:07:23 -0600

    feat: passthrough stdin/stdout

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 fn main() {
     Command::new("cargo")
         .arg("run")
         .arg("-p")
         .arg("bevy_chip8")
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
         .output()
         .expect("Expected cargo to be available/for this crate to be run inside the workspace.");
 }
