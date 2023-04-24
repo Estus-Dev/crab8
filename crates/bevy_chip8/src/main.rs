873874d1e39793b71dc1c566cd19b9a9ccb931d0
Estus <git@estus.dev>
2023-04-23 19:58:27 -0600

    feat: setup window and initial camera

--- crates/bevy_chip8/src/main.rs
+++ crates/bevy_chip8/src/main.rs
@@ -1,9 +1,27 @@
-use bevy::{app::App, DefaultPlugins};
+use bevy::prelude::*;
 use chip_8::Chip8;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "CHIP-8".to_string(),
+                resolution: (1024.0, 512.0).into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .insert_resource(Chip8::default())
+        .add_startup_system(setup_chip8)
         .run();
+}
+
+fn setup_chip8(mut commands: Commands) {
+    commands.spawn(Camera2dBundle {
+        projection: OrthographicProjection {
+            scale: 0.125,
+            ..default()
+        },
+        ..default()
+    });
 }
