014df0f24079c02f97a7d820a4157ffb8593aa01
Estus <git@estus.dev>
2023-04-28 00:21:55 -0600

    build: add log statements to wasm build script

--- crates/crab8_bevy_frontend/scripts/build-wasm.sh
+++ crates/crab8_bevy_frontend/scripts/build-wasm.sh
@@ -12,8 +12,13 @@ ARCH=wasm32-unknown-unknown
 
 cd $SCRIPT_DIR/..
 
+echo "Building..."
 cargo build -p $CRATE_NAME --release --target $ARCH
+
+echo "Generating bindings..."
 wasm-bindgen --out-dir $OUT_DIR --target web $TARGET_DIR/$ARCH/$BUILD_MODE/$CRATE_NAME.wasm
+
+echo "Copying assets..."
 cp -r ./assets/ ./wasm/assets/
 
 cd $PWD
