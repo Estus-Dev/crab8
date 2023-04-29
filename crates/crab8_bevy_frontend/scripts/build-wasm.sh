7fa800dc254a84d284f54eb840a6831dd9477247
Estus <git@estus.dev>
2023-04-29 06:59:21 -0600

    ci: don't fail if no directory to delete

--- crates/crab8_bevy_frontend/scripts/build-wasm.sh
+++ crates/crab8_bevy_frontend/scripts/build-wasm.sh
@@ -19,7 +19,7 @@ echo "Generating bindings..."
 wasm-bindgen --out-dir $OUT_DIR --target web $TARGET_DIR/$ARCH/$BUILD_MODE/$CRATE_NAME.wasm
 
 echo "Copying assets..."
-rm -r ./wasm/assets/
+rm -r ./wasm/assets/ || true
 cp -r ./assets/ ./wasm/assets/
 ls ./wasm/assets/
 
