774bcc9b30f8fbb775dcb027130a353d8ac2b4df
Estus <git@estus.dev>
2023-04-29 08:00:48 -0600

    ci: remove debug ls, I've tracked the issue

--- crates/crab8_bevy_frontend/scripts/build-wasm.sh
+++ crates/crab8_bevy_frontend/scripts/build-wasm.sh
@@ -21,6 +21,5 @@ wasm-bindgen --out-dir $OUT_DIR --target web $TARGET_DIR/$ARCH/$BUILD_MODE/$CRAT
 echo "Copying assets..."
 rm -r ./wasm/assets/ || true
 cp -r ./assets/ ./wasm/assets/
-ls ./wasm/assets/
 
 cd $PWD
