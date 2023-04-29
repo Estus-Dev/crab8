4e0022962404b3dd094501389af916b64b96fef3
Estus <git@estus.dev>
2023-04-29 06:42:43 -0600

    build: clear out wasm assets upon rebuild

--- crates/crab8_bevy_frontend/scripts/build-wasm.sh
+++ crates/crab8_bevy_frontend/scripts/build-wasm.sh
@@ -19,6 +19,7 @@ echo "Generating bindings..."
 wasm-bindgen --out-dir $OUT_DIR --target web $TARGET_DIR/$ARCH/$BUILD_MODE/$CRATE_NAME.wasm
 
 echo "Copying assets..."
+rm -r ./wasm/assets/
 cp -r ./assets/ ./wasm/assets/
 
 cd $PWD
