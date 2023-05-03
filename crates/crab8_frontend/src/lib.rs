7c524336e272358aed47e92f5619297adff0c3d3
Estus <git@estus.dev>
2023-05-03 07:00:42 -0600

    refactor: move window update logic to window mod

--- crates/crab8_frontend/src/lib.rs
+++ crates/crab8_frontend/src/lib.rs
@@ -2,56 +2,24 @@ mod screen;
 mod window;
 
 use window::Crab8Window;
-use winit::{event::Event, event_loop::ControlFlow};
-use winit_input_helper::WinitInputHelper;
+use winit::{event::Event, event_loop::EventLoop};
 
 use crate::screen::DrawScreen;
 pub fn run() {
     let crab8 = crab8::Crab8::default();
+    let event_loop = EventLoop::new();
 
     let mut window = {
         let (crab8_width, crab8_height) = crab8.screen.size();
-        Crab8Window::new(crab8_width as u32, crab8_height as u32).expect("Failed to build window")
+        Crab8Window::new(crab8_width as u32, crab8_height as u32, &event_loop)
+            .expect("Failed to build window")
     };
 
-    let mut input = WinitInputHelper::new();
-
-    window.event_loop.run(move |event, _, control_flow| {
-        if input.update(&event) {
-            if input.close_requested() || input.destroyed() {
-                *control_flow = ControlFlow::Exit;
-            }
-
-            if let Some(size) = input.window_resized() {
-                if window
-                    .pixels
-                    .resize_surface(size.width, size.height)
-                    .is_err()
-                {
-                    *control_flow = ControlFlow::Exit;
-                }
-            }
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::RedrawRequested(_) = event {
+            crab8.screen.draw_screen(window.pixels.frame_mut());
         }
 
-        // I also plan to expand this match statement clippy
-        #[allow(clippy::single_match)]
-        match event {
-            Event::RedrawRequested(_) => {
-                crab8.screen.draw_screen(window.pixels.frame_mut());
-
-                let render_result = window
-                    .pixels
-                    .render_with(|encoder, render_target, context| {
-                        context.scaling_renderer.render(encoder, render_target);
-
-                        Ok(())
-                    });
-
-                if let Err(_err) = render_result {
-                    *control_flow = ControlFlow::Exit;
-                }
-            }
-            _ => (),
-        }
+        window.update(&event, control_flow);
     });
 }
--- crates/crab8_frontend/src/window.rs
+++ crates/crab8_frontend/src/window.rs
@@ -1,34 +1,38 @@
 use pixels::{Pixels, SurfaceTexture};
 use winit::{
     dpi::LogicalSize,
-    event_loop::EventLoop,
-    window::{Window, WindowBuilder},
+    event::Event,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
 };
+use winit_input_helper::WinitInputHelper;
 
 const WIDTH: f64 = 1024.0;
 const HEIGHT: f64 = 512.0;
 
 pub struct Crab8Window {
-    pub event_loop: EventLoop<()>,
+    input: WinitInputHelper,
     pub pixels: Pixels,
-    pub winit: Window,
 }
 
 impl Crab8Window {
-    pub fn new(crab8_width: u32, crab8_height: u32) -> Result<Crab8Window, pixels::Error> {
-        let event_loop = EventLoop::new();
+    pub fn new(
+        crab8_width: u32,
+        crab8_height: u32,
+        event_loop: &EventLoop<()>,
+    ) -> Result<Crab8Window, pixels::Error> {
         let window_size = LogicalSize::new(WIDTH, HEIGHT);
 
         let winit = WindowBuilder::new()
             .with_title("CRAB-8")
             .with_min_inner_size(window_size)
             .with_inner_size(window_size)
-            .build(&event_loop)
+            .build(event_loop)
             .expect("Failed to build winit window");
 
         let surface = SurfaceTexture::new(crab8_width, crab8_height, &winit);
 
-        let mut egui_state = egui_winit::State::new(&event_loop);
+        let mut egui_state = egui_winit::State::new(event_loop);
         egui_state.set_pixels_per_point(winit.scale_factor() as f32);
 
         let mut pixels = Pixels::new(crab8_width, crab8_height, surface)?;
@@ -36,9 +40,39 @@ impl Crab8Window {
         pixels.resize_surface(window_inner_size.width, window_inner_size.height)?;
 
         Ok(Crab8Window {
-            event_loop,
             pixels,
-            winit,
+            input: WinitInputHelper::new(),
         })
+    }
+
+    pub fn update(&mut self, event: &Event<()>, control_flow: &mut ControlFlow) {
+        if self.input.update(event) {
+            if self.input.close_requested() || self.input.destroyed() {
+                *control_flow = ControlFlow::Exit;
+            }
+
+            if let Some(size) = self.input.window_resized() {
+                if self.pixels.resize_surface(size.width, size.height).is_err() {
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+        }
+
+        // I also plan to expand this match statement clippy
+        #[allow(clippy::single_match)]
+        match event {
+            Event::RedrawRequested(_) => {
+                let render_result = self.pixels.render_with(|encoder, render_target, context| {
+                    context.scaling_renderer.render(encoder, render_target);
+
+                    Ok(())
+                });
+
+                if let Err(_err) = render_result {
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+            _ => (),
+        }
     }
 }
