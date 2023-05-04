cae81fbc2a1cbe6b1a3d892c1e012de85b9d5fba
Estus <git@estus.dev>
2023-05-04 15:37:45 -0600

    feat: just run one cycle per frame

--- crates/crab8_frontend/src/lib.rs
+++ crates/crab8_frontend/src/lib.rs
@@ -6,13 +6,18 @@ mod window;
 #[cfg(target_arch = "wasm32")]
 mod wasm;
 
+use std::time::{Duration, Instant};
 use window::Crab8Window;
 use winit::{event::Event, event_loop::EventLoop};
 
 use crate::screen::DrawScreen;
 pub async fn run() {
-    let crab8 = crab8::Crab8::default();
+    let mut crab8 = crab8::Crab8::default();
     let event_loop = EventLoop::new();
+    let mut last_update = Instant::now();
+
+    let test_rom = include_bytes!("../../../roms/1-chip8-logo.ch8");
+    crab8.load(test_rom);
 
     let mut window = {
         let (crab8_width, crab8_height) = crab8.screen.size();
@@ -22,8 +27,24 @@ pub async fn run() {
     };
 
     event_loop.run(move |event, _, control_flow| {
-        if let Event::RedrawRequested(_) = event {
-            crab8.screen.draw_screen(window.pixels.frame_mut());
+        match event {
+            Event::RedrawRequested(_) => {
+                crab8.screen.draw_screen(window.pixels.frame_mut());
+            }
+
+            Event::MainEventsCleared => {
+                let now = Instant::now();
+                let delta_time = Instant::now() - last_update;
+                last_update = now;
+
+                if delta_time > Duration::from_millis(18) {
+                    println!("Long frame detected: {delta_time:?}");
+                }
+
+                crab8.execute(crab8::input::Input::default());
+            }
+
+            _ => (),
         }
 
         window.update(&event, control_flow);
