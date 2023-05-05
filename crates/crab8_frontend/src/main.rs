22a9c535d24664ae3af6b27c3baa857d480f49a2
Estus <git@estus.dev>
2023-05-05 13:31:50 -0600

    fix: refactor main function to work on wasm

--- crates/crab8_frontend/src/main.rs
+++ crates/crab8_frontend/src/main.rs
@@ -1,4 +1,10 @@
+#[cfg(not(target_arch = "wasm32"))]
 #[tokio::main]
 async fn main() {
     crab8_frontend::run().await;
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    wasm_bindgen_futures::spawn_local(crab8_frontend::wasm::run());
 }
