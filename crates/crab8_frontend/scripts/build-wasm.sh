7f6950da0a112cf451c876ec000ce67b4a066742
Estus <git@estus.dev>
2023-05-05 14:09:21 -0600

    build: don't copy assets that don't exist

--- crates/crab8_frontend/scripts/build-wasm.sh
+++ crates/crab8_frontend/scripts/build-wasm.sh
@@ -18,8 +18,4 @@ cargo build -p $CRATE_NAME --release --target $ARCH
 echo "Generating bindings..."
 wasm-bindgen --out-dir $OUT_DIR --target web $TARGET_DIR/$ARCH/$BUILD_MODE/$CRATE_NAME.wasm
 
-echo "Copying assets..."
-rm -r ./wasm/assets/ || true
-cp -r ./assets/ ./wasm/assets/
-
 cd $PWD
