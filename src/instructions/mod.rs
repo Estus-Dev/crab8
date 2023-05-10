aec05a341d44b4e8682afae54722cb5f9150f24a
Estus <git@estus.dev>
2023-05-10 14:37:46 -0600

    refactor: move jump instructions to module

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,5 +1,6 @@
 mod bitwise;
 mod conditional;
+mod jump;
 mod screen;
 
 use rand::random;
@@ -259,23 +260,6 @@ impl Crab8 {
         }
     }
 
-    fn exec_return(&mut self) {
-        let address = self.stack.pop().unwrap_or(Address::default());
-
-        self.program_counter.set(address);
-    }
-
-    fn exec_jump(&mut self, address: Address) {
-        self.program_counter.set(address);
-    }
-
-    fn exec_call(&mut self, address: Address) {
-        self.stack
-            .push(self.program_counter)
-            .expect("Stack Overflow");
-        self.program_counter.set(address);
-    }
-
     fn exec_store(&mut self, register: Register, value: u8) {
         self.registers.set(register, value);
     }
@@ -326,14 +310,6 @@ impl Crab8 {
 
     fn exec_store_address(&mut self, address: Address) {
         self.address_register.set(address);
-    }
-
-    fn exec_jump_offset(&mut self, address: Address) {
-        let offset = self.registers.get(V0);
-        // UNDEFINED BEHAVIOR: I'm choosing to implement overflow by wrapping.
-        let result = address.wrapping_add(offset as u16);
-
-        self.program_counter.set(result);
     }
 
     fn exec_rand(&mut self, register: Register, bitmask: u8) {
@@ -409,46 +385,6 @@ impl Crab8 {
 mod test {
     use super::Instruction::*;
     use crate::{memory::FIRST_CHAR_ADDRESS, prelude::*};
-
-    #[test]
-    fn test_jump() {
-        let cases = [0x1000, 0x1234, 0x1FFF, 0x1CED, 0x12BA];
-
-        let mut crab8 = Crab8::default();
-
-        assert_eq!(crab8.program_counter.get(), 0x200);
-
-        for instruction in cases {
-            crab8.exec(instruction);
-            assert_eq!(crab8.program_counter.get(), instruction & 0x0FFF);
-        }
-    }
-
-    #[test]
-    fn test_call() {
-        let cases = [0x2000, 0x2234, 0x2FFF, 0x2CED, 0x22BA];
-
-        let mut crab8 = Crab8::default();
-
-        for instruction in cases {
-            crab8.exec(instruction);
-            assert_eq!(crab8.program_counter.get(), instruction & 0x0FFF);
-        }
-
-        for (i, address) in cases.iter().map(|a| a & 0x0FFF).rev().skip(1).enumerate() {
-            crab8.exec(Return);
-
-            assert_eq!(crab8.program_counter.get(), address, "{i}");
-        }
-
-        crab8.exec(Return);
-
-        assert_eq!(crab8.program_counter, Address::initial_instruction());
-
-        crab8.exec(Return);
-
-        assert_eq!(crab8.program_counter, Address::default());
-    }
 
     #[test]
     fn test_store() {
@@ -608,24 +544,6 @@ mod test {
         crab8.exec(StoreAddress(0x14E.into()));
 
         assert_eq!(crab8.address_register, 0x14E.into());
-    }
-
-    #[test]
-    fn test_jump_offset() {
-        let cases = [
-            (0xB000u16, 0x00u8, 0x000u16),
-            (0xB123, 0x00, 0x123),
-            (0xB123, 0x45, 0x168),
-        ];
-
-        let mut crab8 = Crab8::default();
-
-        for (instruction, offset, expected) in cases {
-            crab8.registers.set(V0, offset);
-            crab8.exec(instruction);
-
-            assert_eq!(crab8.program_counter.get(), expected);
-        }
     }
 
     #[test]
