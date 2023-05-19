0ab3d69da8b1da126839c3c5e49d583d8a7c1a6e
Estus <git@estus.dev>
2023-05-19 17:07:14 -0600

    refactor: move all lexing over to logos

--- crates/crab8_asm/src/lexer.rs
+++ crates/crab8_asm/src/lexer.rs
@@ -1,73 +1,10 @@
-use crate::token::Token;
 use crab8::registers::Register;
-
-pub fn lex(input: &str) -> Vec<Token> {
-    // This is a bit overkill on preallocation, but at least it won't have to keep reallocating.
-    let mut tokens = Vec::with_capacity(input.len());
-
-    for line in input.lines() {
-        let mut iter = line.chars().peekable();
-
-        while let Some(&char) = iter.peek() {
-            if char.is_whitespace() {
-                iter.next();
-                continue;
-            }
-            let token: String = iter
-                .clone()
-                .take_while(|next| !next.is_whitespace())
-                .collect();
-            let length = token.len();
-
-            tokens.push(match token.as_str() {
-                // "v0" => Token::Register(Register::V0),
-                // "v1" => Token::Register(Register::V1),
-                // "v2" => Token::Register(Register::V2),
-                // "v3" => Token::Register(Register::V3),
-                // "v4" => Token::Register(Register::V4),
-                // "v5" => Token::Register(Register::V5),
-                // "v6" => Token::Register(Register::V6),
-                // "v7" => Token::Register(Register::V7),
-                // "v8" => Token::Register(Register::V8),
-                // "v9" => Token::Register(Register::V9),
-                // "va" => Token::Register(Register::VA),
-                // "vb" => Token::Register(Register::VB),
-                // "vc" => Token::Register(Register::VC),
-                // "vd" => Token::Register(Register::VD),
-                // "ve" => Token::Register(Register::VE),
-                // "vf" => Token::Register(Register::VF),
-                ":=" => Token::Assign,
-                "+=" => Token::Add,
-                "-=" => Token::Sub,
-                "=-" => Token::SubFrom,
-                "&=" => Token::And,
-                "|=" => Token::Or,
-                "^=" => Token::Xor,
-                "<<=" => Token::LShift,
-                ">>=" => Token::RShift,
-                "==" => Token::Eq,
-                "!=" => Token::Neq,
-                "<" => Token::Lt,
-                ">" => Token::Gt,
-                "<=" => Token::Lte,
-                ">=" => Token::Gte,
-                "key" => Token::Key,
-                "-key" => Token::NKey,
-                _ => Token::Unknown,
-            });
-
-            for _ in 0..length {
-                iter.next();
-            }
-        }
-    }
-
-    tokens
-}
+use logos::Logos;
 
 #[cfg(test)]
 mod test {
-    use logos::Logos;
+
+    use crate::token::Token;
 
     use super::*;
 
@@ -128,55 +65,87 @@ mod test {
     #[test]
     fn test_lex_assign() {
         let input = ":=";
-        let expected = vec![Token::Assign];
+        let mut lexer = Token::lexer(input);
 
-        assert_eq!(lex(input), expected);
+        assert_eq!(lexer.next(), Some(Ok(Token::Assign)));
+        assert_eq!(lexer.slice(), ":=");
     }
 
     #[test]
     fn test_lex_basic_math_ops() {
         let input = "+= -= =-";
-        let expected = vec![Token::Add, Token::Sub, Token::SubFrom];
+        let mut lexer = Token::lexer(input);
 
-        assert_eq!(lex(input), expected);
+        assert_eq!(lexer.next(), Some(Ok(Token::Add)));
+        assert_eq!(lexer.slice(), "+=");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Sub)));
+        assert_eq!(lexer.slice(), "-=");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::SubFrom)));
+        assert_eq!(lexer.slice(), "=-");
     }
 
     #[test]
     fn test_lex_bitwise_ops() {
         let input = "&= |= ^=";
-        let expected = vec![Token::And, Token::Or, Token::Xor];
+        let mut lexer = Token::lexer(input);
 
-        assert_eq!(lex(input), expected);
+        assert_eq!(lexer.next(), Some(Ok(Token::And)));
+        assert_eq!(lexer.slice(), "&=");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Or)));
+        assert_eq!(lexer.slice(), "|=");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Xor)));
+        assert_eq!(lexer.slice(), "^=");
     }
 
     #[test]
     fn test_lex_shift_ops() {
         let input = "<<= >>=";
-        let expected = vec![Token::LShift, Token::RShift];
+        let mut lexer = Token::lexer(input);
 
-        assert_eq!(lex(input), expected);
+        assert_eq!(lexer.next(), Some(Ok(Token::LShift)));
+        assert_eq!(lexer.slice(), "<<=");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::RShift)));
+        assert_eq!(lexer.slice(), ">>=");
     }
 
     #[test]
     fn test_lex_comparison_ops() {
         let input = "== != <  >  <= >=";
-        let expected = vec![
-            Token::Eq,
-            Token::Neq,
-            Token::Lt,
-            Token::Gt,
-            Token::Lte,
-            Token::Gte,
-        ];
+        let mut lexer = Token::lexer(input);
 
-        assert_eq!(lex(input), expected);
+        assert_eq!(lexer.next(), Some(Ok(Token::Eq)));
+        assert_eq!(lexer.slice(), "==");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Neq)));
+        assert_eq!(lexer.slice(), "!=");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Lt)));
+        assert_eq!(lexer.slice(), "<");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Gt)));
+        assert_eq!(lexer.slice(), ">");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Lte)));
+        assert_eq!(lexer.slice(), "<=");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Gte)));
+        assert_eq!(lexer.slice(), ">=");
     }
 
     #[test]
     fn test_lex_input_ops() {
         let input = "key -key";
-        let expected = vec![Token::Key, Token::NKey];
+        let mut lexer = Token::lexer(input);
 
-        assert_eq!(lex(input), expected);
+        assert_eq!(lexer.next(), Some(Ok(Token::Key)));
+        assert_eq!(lexer.slice(), "key");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::NKey)));
+        assert_eq!(lexer.slice(), "-key");
     }
 }
