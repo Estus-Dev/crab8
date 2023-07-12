113c2ae3c843a63617d8045ebb2b29fd91376fc7
Estus <git@estus.dev>
2023-07-11 18:00:29 -0600

    test: include disassembly in register dump for now

--- tests/timendus.rs
+++ tests/timendus.rs
@@ -10,7 +10,7 @@ fn timendus_1_chip8_logo() {
     let expected_screen = include_str!("./expected/1-chip8-logo.ch8s");
     let expected_screen = Screen::from_str(expected_screen).unwrap();
     let expected_registers = "0-F: 30 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ".to_owned()
-        + "D: 00 S: 00 CS: 00 I: 02F5 (CE 87 03 03) PC: 024E (12 4E 0F 02)";
+        + "D: 00 S: 00 CS: 00 I: 02F5 (CE 87 03 03) PC: 024E (12 4E 0F 02) - jump 0x24E";
 
     let mut crab8 = Crab8::new();
 
@@ -27,7 +27,7 @@ fn timendus_2_ibm_logo() {
     let expected_screen = include_str!("./expected/2-ibm-logo.ch8s");
     let expected_screen = Screen::from_str(expected_screen).unwrap();
     let expected_registers = "0-F: 31 08 00 00 00 00 00 00 00 00 00 00 00 00 00 00".to_owned()
-        + " D: 00 S: 00 CS: 00 I: 0275 (E5 05 E2 00) PC: 0228 (12 28 FF 00)";
+        + " D: 00 S: 00 CS: 00 I: 0275 (E5 05 E2 00) PC: 0228 (12 28 FF 00) - jump 0x228";
 
     let mut crab8 = Crab8::new();
 
@@ -44,7 +44,7 @@ fn timendus_3_corax_plus() {
     let expected_screen = include_str!("./expected/3-corax+.ch8s");
     let expected_screen = Screen::from_str(expected_screen).unwrap();
     let expected_registers = "0-F: FB 03 07 00 00 2A 05 EC 32 36 3B 10 00 00 00 00".to_owned()
-        + " D: 00 S: 00 CS: 00 I: 0465 (00 A0 C0 80) PC: 045C (14 5C 01 03)";
+        + " D: 00 S: 00 CS: 00 I: 0465 (00 A0 C0 80) PC: 045C (14 5C 01 03) - jump 0x45C";
 
     let mut crab8 = Crab8::new();
 
@@ -62,7 +62,7 @@ fn timendus_5_quirks() {
     let expected_screen = include_str!("./expected/5-quirks.ch8s");
     let expected_screen = Screen::from_str(expected_screen).unwrap();
     let expected_registers = "0-F: 40 40 40 00 3C 08 00 3C 0C 00 58 64 84 38 1A 00".to_owned()
-        + " D: 00 S: 00 CS: 00 I: 062D (E0 80 C0 80) PC: 05D2 (F0 0A 22 02)";
+        + " D: 00 S: 00 CS: 00 I: 062D (E0 80 C0 80) PC: 05D2 (F0 0A 22 02) - v0 := key";
 
     let mut crab8 = Crab8::new();
 
