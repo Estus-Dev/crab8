f1cab320bdb25626a3fff81744357f4b23c344bc
Estus <git@estus.dev>
2023-07-12 15:00:10 -0600

    feat: impl Display for Instruction

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -310,6 +310,12 @@ impl Debug for Instruction {
     }
 }
 
+impl Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(self, f)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::Instruction::*;
